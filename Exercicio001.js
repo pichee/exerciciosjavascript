ff4ca224a7c0e431dc6196904ed8b3bf75b43fd2
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-05 10:21:25 -0300

    Atualizar o Exercicio001.js

--- Exercicio001.js
+++ Exercicio001.js
@@ -1,1 +1,5 @@
-p
+//Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+let idade=18
+let peso=60.5
+console.log("Idade:",idade)
+console.log("Idade:",peso)
