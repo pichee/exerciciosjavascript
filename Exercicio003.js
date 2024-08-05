b67a0c07ab777730ff0426f062463c0db30c068a
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-05 11:25:36 -0300

    Atualizar o

---
+++ Exercicio003.js
@@ -0,0 +1,3 @@
+//Exercício 3: Declare uma variável chamada "númeroMáximo", atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.
+let NUMERO_MAXIMO=Number.MAX_VALUE;
+console.log("Numero maximo",NUMERO_MAXIMO);
