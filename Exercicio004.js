1a15abf73a9341a3c54809ce582a7e3f97e229a7
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-05 12:05:45 -0300

    Atualizar o

---
+++ Exercicio004.js
@@ -0,0 +1,5 @@
+//Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+let nome="Gustavo";
+let sobrenome="Pich";
+let nomecompleto=nome+""+sobrenome;
+console.log("Nome Completo:",nomecompleto);
