276f3eb46aeeec89cbfbb0d491af39953f056d70
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-07 09:16:36 -0300

    Atualizar o

---
+++ Exercicio006.js
@@ -0,0 +1,6 @@
+//Exercício 6: Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT
+let boleanou=true;
+let boleanou2=false;
+console.log("E:",boleanou&&boleanou2);
+console.log("Ou:",boleanou||boleanou2);
+console.log("Não:",!boleanou)
