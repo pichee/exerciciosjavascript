1969bf5ac410ae47785dfc7b832b1c2bb3afc315
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-08 10:14:01 -0300

    Atualizar o

---
+++ Exercicio007.js
@@ -0,0 +1,7 @@
+//Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados:
+let numero1=2
+let numero2=3
+let numero3=5
+console.log("numero1=numero2:",numero1==numero2)
+console.log("numero3>numero1:",numero3>numero1)
+console.log("Numero1>numero2:",numero1>numero2)
