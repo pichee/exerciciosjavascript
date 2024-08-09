8debe8f2facf4506459592adaab8395a68546a87
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-09 20:04:22 -0300

    Create Exercicio012.js

---
+++ Exercicio012.js
@@ -0,0 +1,15 @@
+let altura=prompt("Digite Sua altura:");
+let peso=prompt("Digite seu peso:");
+let imc=peso/altura*altura;
+function dizerseuimc(imc){
+    if(imc<=16.9){
+        print("Peso muito abaixo do peso");
+    }
+    else if(imc>=17 and imc<18.4){
+        print("Abaixo do peso");
+    }
+    else if(imc>=18.4 and imc=<18.4){
+        print("Abaixo do peso");
+    }
+}
+console.log("Teste");
