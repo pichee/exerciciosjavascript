1781ed264edf91d762110d06d4202ff53aa141ce
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-12 09:04:13 -0300

    Atualizar o

---
+++ Exercicio013.js
@@ -0,0 +1,2 @@
+//Exercício 13: Escreva um programa que calcula o IMC (Índice de Massa Corporal) de uma pessoa e imprime uma mensagem
+function calcularIMC (peso, altura) { var imc = peso / (altura * altura); if (imc < 18.5 ) { console .log( "Abaixo do peso" ); } else if (imc >= 18.5 && imc < 25 ) { console .log( "Peso normal" ); } else if (imc >= 25 && imc < 30 ) { console .log( "Sobrepeso" ); } else { console .log( "Obesidade" ); } }
