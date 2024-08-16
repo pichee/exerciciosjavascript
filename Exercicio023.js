41a77e0c0e6c3da82901048bf8fe14dd01b6afcf
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-16 16:23:52 -0300

    Atualizar o

---
+++ Exercicio023.js
@@ -0,0 +1,3 @@
+//Exercício 23: Escreva um programa que verifica as cores de um semafáro.
+
+function verificaSemafaro(cor) { if (cor === "verde") { console .log( "Siga em frente." ); } else if (cor === "amarelo") { console .log( "Atenção, diminua a velocidade." ); } else if (cor === "vermelho") { console .log( "Pare seu veículo." ); } else { console .log( "Envie uma cor válida." ); } } verificaSemafaro( "verde" ); // Imprime: Siga em frente verificaSemafaro( "amarelo" ); // Imprime: Atenção, diminua a velocidade verificaSemafaro( "vermelho" ); // Imprime: Pare seu veículo verificaSemafaro( "azul" ); // Impime: Envie uma cor válida
