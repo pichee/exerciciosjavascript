1a0971f9bd36e86efb2b51ff007f3dae6b826240
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-16 16:21:05 -0300

    Atualizar o

---
+++ Exercicio021.js
@@ -0,0 +1,45 @@
+//Exercício 21: Escreva um programa que determina a estação do ano com base no mês.
+function meses() {
+  mes=prompt("Digite um número de 1 a 12:")
+  switch (parseInt(mes)) {
+    case 1:
+      console.log("Janeiro");
+      break;
+    case 2:
+      console.log("Fevereiro");
+      break;
+    case 3:
+      console.log("Março");
+      break;
+    case 4:
+      console.log("Abril");
+      break;
+    case 5:
+      console.log("Maio");
+      break;
+    case 6:
+      console.log("Junho");
+      break;
+    case 7:
+      console.log("Julho");
+      break;
+    case 8:
+      console.log("Agosto");
+      break;
+    case 9:
+      console.log("Setembro");
+      break;
+    case 10:
+      console.log("Outubro");
+      break;
+    case 11:
+      console.log("Novembro");
+      break;
+    case 12:
+      console.log("Dezembro");
+      break;
+    default:
+      console.log("Número inválido. Digite um número de 1 a 12.");
+  }
+}
+meses()
