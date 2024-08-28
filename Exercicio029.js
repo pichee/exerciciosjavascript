4bfb2a1a575dd6e03a54e595ebe05f3a0e4861dd
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-28 09:23:52 -0300

    Atualizar o

---
+++ Exercicio029.js
@@ -0,0 +1,8 @@
+//Exercício 29: Usando um laço do-while, imprima todos os números pares de 1 a 100.
+let numeros=1;
+while(numeros<=100){
+  if(numeros%2==0){
+    console.log(numeros)
+  }
+  numeros++;
+}
