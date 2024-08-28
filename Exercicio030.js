1f3ece81512c79f260dacc7cc65136cb96c86aa4
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-28 17:16:32 -0300

    Atualizar o

---
+++ Exercicio030.js
@@ -0,0 +1,1 @@
+for ( let num = 2 ; num <= 100 ; num++) { let isPrime = true ; for ( let div = 2 ; div < num; div++) { if (num % div == 0 ){ isPrime = false ; break ; } } if (isPrime){ console .log(num); } }
