207889cd24e25baa27c3d6ba66b88d81893cd11d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-28 09:08:56 -0300

    Atualizar o

---
+++ Exercicio028.c
@@ -0,0 +1,8 @@
+//Exercício 28: Usando um laço while, calcule e imprima a soma de todos os números de 1 a 100.
+let soma=0;
+let numeros=1;
+while(numeros<=100){
+  soma=soma+numeros;
+  numeros++;
+}
+console.log(soma);
